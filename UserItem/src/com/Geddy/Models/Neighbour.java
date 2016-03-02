84b780cf8c2c261db0b6620315de27ce4d8e4cbb
Geddy Schellevis <geddyschellevis@gmail.com>
2016-03-02 22:53:22 +0100

    Add recommendations

---
+++ UserItem/src/com/Geddy/Models/Neighbour.java
@@ -0,0 +1,34 @@
+package com.Geddy.Models;
+
+import com.Geddy.Similarity.Distance;
+
+/**
+ * Created by geddy on 02/03/16.
+ */
+public class Neighbour {
+
+    private UserPreference user;
+    private Double distance;
+    private Double weight;
+
+    public Neighbour(UserPreference user, Double distance){
+        this.user = user;
+        this.distance = distance;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public UserPreference getUser() {
+        return user;
+    }
+}
